7b558b524b19432c407686e2f397e8d785eb16b1
Adam K <adamkunkel54@gmail.com>
2022-01-28 14:04:35 -0500

    Added data, script, and notes/outline doc

---
+++ analysis/scripts/data_cleaning.R
@@ -0,0 +1,42 @@
+d = `2010.01.01.2022.01.28`
+`2010.01.01.2022.01.28` = NULL
+
+library(tidyverse)
+
+a = NULL
+a = str_extract_all(d$notes, "Wagner")
+a = str_detect(d$notes, regex("Wagner", ignore_case = TRUE))
+
+
+a = data.frame(a)
+a <- tibble::rowid_to_column(a, "id")
+d <- tibble::rowid_to_column(d, "id")
+
+
+
+b = subset(a, a == "TRUE")
+
+c = left_join(b, d)
+
+
+
+
+a = NULL
+a = str_detect(d$notes, regex("", ignore_case = TRUE))
+
+
+a = data.frame(a)
+a <- tibble::rowid_to_column(a, "id")
+d <- tibble::rowid_to_column(d, "id")
+
+
+
+b = subset(a, a == "TRUE")
+
+c = left_join(b, d)
+
+
+a$num
+
+
+a
