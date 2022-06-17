1bb18cb400bc2ecf8226c10ae2d34fa95a127500
Sky K <adamkunkel54@gmail.com>
2022-06-17 17:28:41 -0400

    Create clean_data and delete data_cleaning

---
+++ scripts/clean_data.R
@@ -0,0 +1,5 @@
+# Sky Kunkel #
+# The Wagner Group and Violence: Russia's Re-Entry Into Cold War African Politics #
+# Data Cleaning #
+
+d <- read.csv("./data/car_wagner_analysis/data/1900-01-01-2022-06-16-Central_African_Republic.csv")
