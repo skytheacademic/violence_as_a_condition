0543b518f5fe3681216c5ec39ed41de52a35d2eb
Sky K <adamkunkel54@gmail.com>
2022-07-08 10:46:53 -0400

    Update clean_data.R

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -5,7 +5,7 @@ library(tidyverse)
 setwd("../")
 
 # load data #
-d <- read.csv("./data/1900-01-01-2022-06-16-Central_African_Republic.csv") %>%
+d <- read.csv("./data/acled/1900-01-01-2022-06-16-Central_African_Republic.csv") %>%
   select(-c(data_id, iso, event_id_cnty, event_id_no_cnty, 
             region, source, source_scale, timestamp))
 table(d$actor1)
@@ -31,3 +31,6 @@ b = str_detect(d$notes, regex("Wagner", ignore_case = TRUE))
 
 # IMPORTANT #
 ### Eventually will need to find a way to distinguish these; ignore for now ###
+
+
+df = subset(d, d$actor1 == "Wagner Group")
