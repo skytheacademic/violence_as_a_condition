bc282cbbbbeb1f218dec85ba065379a2c230ec62
Sky K <adamkunkel54@gmail.com>
2022-09-13 15:38:14 -0400

    Update clean_data.R
    small cleaning, also added code for an RDD in case I end up doing that one

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -2,9 +2,11 @@
 # The Wagner Group and Violence: Russia's Re-Entry Into Cold War African Politics #
 # Data Cleaning #
 library(tidyverse); library(lubridate)
+library(rdrobust); library(rdd)
 setwd("../")
 options(scipen = 999) # turn off scientific notation
 
+#### Clean Data ####
 # load data #
 d = read.csv("./data/acled/1900-01-01-2022-09-01-Central_African_Republic.csv") %>%
   select(-c(iso, event_id_cnty, event_id_no_cnty, 
@@ -47,6 +49,8 @@ min(d$event_date[d$t_ind == 1]) # establish first time period to receive "treatm
 d = subset(d, d$event_date > "2018-04-07") # subset to all data after 2018-04-07
 range(d$event_date) # verify it worked
 
+
+#### Instrumental variable ####
 # make the instrument
 table(d$t_ind[d$event_date> "2021-11-01"])
 d$iv = 0
@@ -61,10 +65,37 @@ reg2 <- lm(d$fatalities ~ instrumented.trt) # Second stage
 summary(reg2)
 
 
+#### RDD ####
+date = rep(ymd("2021-11-01"), nrow(d))
+d$score = date - d$event_date
+d$score = as.numeric(d$score)
+#Use the MSE-optimal bandwidth to compute the treatment effect using a triangular kernel.
+h = rdbwselect(d$death, d$score)$bws[1]
+h     
+
+d$kweights = kernelwts(d$score, 0, bw = h, kernel = "triangular")
+sum(d$kweights!=0 & !is.na(d$death))
+plot(d$score,d$kweights)
+
+reg1 = lm(death ~ t_ind + score + t_ind*score, weights = kweights, data = d)
+summary(reg1)
+
+#Provide an RD plot illustrating the treatment effect
+rdplot(y = d$death, x = d$score,  h = h, nbins = 100, subset = -h <= d$score & d$score <= h, 
+       binselect="esmv", kernel="triangular", p=1, title = "", 
+       y.label = "", x.label = "")
+
+summary(rdrobust(d$death,d$score,all=TRUE))
+
+# Density plot to examine sorting
+DCdensity(d$score[abs(d$score)<h],0)
+# big gaps are bad here, and show evidence of sorting
 
 
 
 
+
+#### Plot of treatment by admin? ####
 # group only by admin to see which units were ever treated
 d.ag.test = d %>%
   group_by(admin3) %>%
@@ -80,7 +111,7 @@ d.ag = d %>%
   summarize(death = (death), fatalities = sum(fatalities), battle = sum(battle),
             remote = sum(remote), protest = sum(protest), riot = sum(riot), 
             str_d = sum(str_d), vac = sum(vac))
-###### need to verify that this^^^ worked correctly #######
+# need to verify that this^^^ worked correctly #
 
 
 
